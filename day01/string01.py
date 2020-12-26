a70fc333850cc544aaef21d889650d4c431b0226
LuoKai <kluo@grandstream.cn>
2020-12-26 15:06:59 +0800

    更新字符串笔记

--- day01/string01.py
+++ day01/string01.py
@@ -7,10 +7,10 @@
 # 单引号方式
 str1 = 'I am a string type'
 
-# 双引号方式
+# 双引号方式, 注意双引号里可以有单引号，双引号中的单引号并当成正常的字符串
 str2 = "Today is a good day, I 'm LiLei"
 
-# 三引号方式
+# 三引号方式，三引号多用于文字较多的字符串
 """
 i am line 1
 i am line 2
