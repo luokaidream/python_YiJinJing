34da2269062c200783e7c93a94a0e3c5175a5507
LuoKai <kluo@grandstream.cn>
2020-12-26 15:00:08 +0800

    更新字符串笔记

--- day01/string01.py
+++ day01/string01.py
@@ -2,6 +2,19 @@
 # Author: LuoKai
 # date = 2020/12/26
 
-" I am a string type"
+# 字符串的表示方式，以及用法
+
+# 单引号方式
+str1 = 'I am a string type'
+
+# 双引号方式
+str2 = "Today is a good day, I 'm LiLei"
+
+# 三引号方式
 """
+i am line 1
+i am line 2
+i am line 3
 """
+
+
